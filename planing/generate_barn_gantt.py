ea944ef29fcf9a28adf02088c7276dcc520c5709
TT <tamastorkoly@Mac.lan>
2025-09-30 10:12:49 +0200

    Organize Gantt tooling under planing/

--- planing/generate_barn_gantt.py
+++ planing/generate_barn_gantt.py
@@ -26,6 +26,12 @@ from typing import Dict, Iterable, List, Tuple
 
 import polars as pl
 
+ROOT = Path(__file__).resolve().parents[1]
+import sys
+
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+
 from simulation.config import SimulationConfig
 
 MS_PER_SECOND = 1000
